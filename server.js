efc1e0b30314a9648c92c9c1719a4e040890e634
mrghost077 <msnlms86@gmail.com>
2026-04-02 18:48:46 +0530

    initialising the server

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from "helmet";
+import morgan from "morgan";
+
+// Load environment Variables 
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3301;
+
+// MiddleWares
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.get('/', (req , res) => {
+    res.json({message: 'Pharmacy API is Running..'});
+});
+
+/* 404 */
+app.use((req, res) => {
+  res.status(404).json({ message: "Not Found" });
+});
+
+app.listen(PORT,  () => {
+    console.log(`Server is running on port ${PORT}`);
+});

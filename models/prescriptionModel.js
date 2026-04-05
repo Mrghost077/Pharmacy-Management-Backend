3d606b91838cb7cd344ef101851824071a3ef967
mrghost077 <msnlms86@gmail.com>
2026-04-05 10:17:12 +0530

    polished prescription model

--- models/prescriptionModel.js
+++ models/prescriptionModel.js
@@ -1,10 +1,23 @@
 import mongoose from "mongoose";
 
 const prescriptionSchema = new mongoose.Schema({
-  userId : ObjectId,
-  imageUrl : {type: String},
-  status : {type: String, default: "pending"},
-  createdAt: Date  
+  userId : {
+    type: mongoose.Schema.ObjectId,
+    ref: 'user',
+    required: true
+  },
+
+  imageUrl : {type: String, required: true},
+  publicId: {type: String},
+  status : {
+    type: String,
+    enum: ["pending", "approved", "rejected"],
+    default: "pending"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  } 
 });
 
 const prescriptionModel = mongoose.models.prescription || mongoose.model('prescription', prescriptionSchema);

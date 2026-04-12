df4f3aa3260d5c68eb1e235c77dddbbb381459cc
mrghost077 <msnlms86@gmail.com>
2026-04-12 11:48:35 +0530

    created orderModel

---
+++ models/orderModel.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    userId : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required: true
+    },
+
+    prescriptionId : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "prescription",
+        required : true
+    },
+
+    // patients Input
+    patientNotes : {
+        type : String,
+        maxlength : 500
+    },
+
+    items : [{
+        name : String,
+        brandName : String,
+        isGeneric : {
+            type : Boolean,
+            default : false
+        },
+        unitPrice : Number,
+        quantityRequested : Number,
+        subTotal : Number
+    }],
+
+    totalAmount : Number,
+    orderStatus : {
+        type: String,
+        enum: ["awaiting-quote", "quote-sent", "rejected", "paid", "processing", "delivered"],
+        default: 'awaiting-quote'
+    },
+}, {timestamps : true});
